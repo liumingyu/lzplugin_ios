348abf2f0248975891dea9ea7f2bec76686bbe07
lmy01032117 <lmy01032117@alibaba-inc.com>
2024-12-18 14:29:33 +0800

    Initial Commit

---
+++ lzplugin_ios/lzplugin_ios.h
@@ -0,0 +1,18 @@
+//
+//  lzplugin_ios.h
+//  lzplugin_ios
+//
+//  Created by 刘明雨 on 2024/12/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for lzplugin_ios.
+FOUNDATION_EXPORT double lzplugin_iosVersionNumber;
+
+//! Project version string for lzplugin_ios.
+FOUNDATION_EXPORT const unsigned char lzplugin_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lzplugin_ios/PublicHeader.h>
+
+
---
+++ lzplugin_iosTests/lzplugin_iosTests.m
@@ -0,0 +1,36 @@
+//
+//  lzplugin_iosTests.m
+//  lzplugin_iosTests
+//
+//  Created by 刘明雨 on 2024/12/18.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface lzplugin_iosTests : XCTestCase
+
+@end
+
+@implementation lzplugin_iosTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
